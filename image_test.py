48da2f5619a917d0c9ee4f2290356155c7c2f63f
Ruoming Pang <r_pang@apple.com>
2022-01-15 15:24:41 -0500

    Adds more checks to dataset building.

--- image_test.py
+++ image_test.py
@@ -14,6 +14,7 @@ def _count_batches(dataset, max_batches=100):
 
 
 class ImagenetInputTest(absltest.TestCase):
+
     def testIteration(self):
         cfg = ImagenetInput.default_config().set(
             name="test", tfds_name="imagenet2012", split="train[:40]", global_batch_size=8, is_training=False,
@@ -25,6 +26,13 @@ class ImagenetInputTest(absltest.TestCase):
         for batch in dataset:
             self.assertEqual({'image': (8, 224, 224, 3), 'label': (8,)}, utils.shapes(batch))
             break
+
+    def testIndivisible(self):
+        cfg = ImagenetInput.default_config().set(
+            name="test", tfds_name="imagenet2012", split="train[:31]", global_batch_size=8, is_training=False,
+            data_dir="gs://permanent-us-central1-q5loch/tensorflow_datasets",
+        )
+        dataset = cfg.instantiate(parent=None)
 
 
 if __name__ == "__main__":
