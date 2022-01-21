4b3c5cef1fcdebee8dc6f79ecd50308f08f8284c
Ruoming Pang <r_pang@apple.com>
2022-01-21 11:52:18 -0500

    Makes pjit_test.py use absl.app.

--- pjit_test.py
+++ pjit_test.py
@@ -1,48 +1,55 @@
 """A simple test for pjit on TPU."""
 import jax
 import jax.numpy as jnp
+import numpy as np
+from absl import app
+from jax.experimental import PartitionSpec
 from jax.experimental import maps
 from jax.experimental import mesh_utils
-from jax.experimental import PartitionSpec
 from jax.experimental.pjit import pjit
 from jax.experimental.pjit import with_sharding_constraint
-import numpy as np
-
-print(jax.__version__)
-print(jax.devices())
-
-mesh_shape = (4, 2) if len(jax.devices()) == 8 else (1, 1)
-devices = mesh_utils.create_device_mesh(mesh_shape)
-mesh = maps.Mesh(devices, ("data", "model"))
-print(mesh)
-
-x = np.arange(8 * 2).reshape(8, 2)
-print(x)
-
-rng_key = jax.random.PRNGKey(42)
-params = {}
-param_spec = {}
-for k in ("w1", "w2"):
-    rng_key, sub_key = jax.random.split(rng_key)
-    params[k] = jax.random.normal(sub_key, shape=(2, 6))
-    param_spec[k] = PartitionSpec(None, "model")
-
-x_spec = PartitionSpec("data", None)
-y_spec = PartitionSpec("data", None)
-# y_spec = PartitionSpec('data', 'model')
 
 
-def mlp(params, x):
-    x = x @ params["w1"]
-    x = with_sharding_constraint(x, PartitionSpec("data", "model"))
-    x = jnp.fmax(x, 0)
-    x = x @ jnp.transpose(params["w2"])
-    return x
+def main(argv):
+    print(jax.__version__)
+    print(jax.devices())
+
+    mesh_shape = (4, 2) if len(jax.devices()) == 8 else (1, 1)
+    devices = mesh_utils.create_device_mesh(mesh_shape)
+    mesh = maps.Mesh(devices, ("data", "model"))
+    print(mesh)
+
+    x = np.arange(8 * 2).reshape(8, 2)
+    print(x)
+
+    rng_key = jax.random.PRNGKey(42)
+    params = {}
+    param_spec = {}
+    for k in ("w1", "w2"):
+        rng_key, sub_key = jax.random.split(rng_key)
+        params[k] = jax.random.normal(sub_key, shape=(2, 6))
+        param_spec[k] = PartitionSpec(None, "model")
+
+    x_spec = PartitionSpec("data", None)
+    # y_spec = PartitionSpec("data", None)
+    y_spec = PartitionSpec('data', 'model')
 
 
-f = pjit(mlp, in_axis_resources=(param_spec, x_spec), out_axis_resources=y_spec)
+    def mlp(params, x):
+        x = x @ params["w1"]
+        x = with_sharding_constraint(x, PartitionSpec("data", "model"))
+        x = jnp.fmax(x, 0)
+        x = x @ jnp.transpose(params["w2"])
+        return x
 
-# Sends data to accelerators based on partition_spec
-with maps.mesh(mesh.devices, mesh.axis_names):
-    y = f(params, x)
-print(y)
+
+    f = pjit(mlp, in_axis_resources=(param_spec, x_spec), out_axis_resources=y_spec)
+
+    # Sends data to accelerators based on partition_spec
+    with maps.mesh(mesh.devices, mesh.axis_names):
+        y = f(params, x)
+    print(y)
+
+
+if __name__ == "__main__":
+    app.run(main)
