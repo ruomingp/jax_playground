d99c0073267fc17640717aa2d9a0faec4a3442b0
Ruoming Pang <r_pang@apple.com>
2022-01-22 11:33:40 -0500

    Disables verbose logging of the trainer.

--- imagenet.py
+++ imagenet.py
@@ -6,7 +6,8 @@ dir=gs://${gs_bucket}/${USER}/experiments/imagenet-$(date +%F)b
 echo $dir
 data_dir=gs://${gs_bucket}/tensorflow_datasets
 echo $data_dir
-python3 imagenet.py --dir=$dir --data_dir=$data_dir 2>&1 | gsutil cp - $dir/log-$(date +%F-%T)
+# python3 imagenet.py --dir=$dir --data_dir=$data_dir 2>&1 | gsutil cp - $dir/log-$(date +%F-%T)
+python3 imagenet.py --dir=$dir --data_dir=$data_dir 2>&1 | tee /tmp/log | gsutil cp - $dir/log-$(date +%F-%T)
 
 On your local machine:
 pip install tensorflow tbp-nightly
@@ -108,7 +109,7 @@ def imagenet_trainer_config():
     summary_dir = os.path.join(FLAGS.dir, "summaries")
     cfg.summary_writer.write_every_n_steps = 100
     cfg.summary_writer.dir = os.path.join(summary_dir, "train_train")
-    cfg.vlog = 5
+    cfg.vlog = 0  # Set to 5 to enable verbose logging.
     for evaler_cfg in cfg.evalers:
         evaler_cfg.vlog = 5
         evaler_cfg.run_every_n_steps = FLAGS.interval or steps_per_epoch
